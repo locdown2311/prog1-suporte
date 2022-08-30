28c798f676133e626274310a4169a485c517bc50
IGOR <igor2311@gmail.com>
2022-08-30 17:39:26 -0300

    Adicionado anotação da divisão

--- Prova-1/Lista-4/q14.c
+++ Prova-1/Lista-4/q14.c
@@ -7,6 +7,8 @@ int main(int argc, char const *argv[])
     scanf("%d", &num);
     printf("\nDivisores: ");
 
+
+    //Nenhum numero divide por um que é mais da metade dele mesmo
     for (int i = 1; i <= num / 2; i++)
     {
         if (num % i == 0)
