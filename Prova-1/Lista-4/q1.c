a9a271e953960d8a8e14fc5ab2595688c763d7b9
IGOR <igor2311@gmail.com>
2022-08-30 15:08:53 -0300

    Contador 1 a 10

---
+++ Prova-1/Lista-4/q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int contador = 1;
+    while (contador <=10)
+    {
+        printf("%d ",contador);
+        contador++;
+    }
+    
+    
+    /*
+
+        for (contador = 1; i <= 10; contador++){
+            printf("%d ",contador);
+            contador++;
+
+
+        Outra possibilidade acima
+    }
+
+
+
+    */
+
+
+    return 0;
+}
