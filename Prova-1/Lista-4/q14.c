39f6f5f000234435492e919008c20175419ca9c0
IGOR <igor2311@gmail.com>
2022-08-30 22:04:17 -0300

    Corrigido for

--- Prova-1/Lista-4/q14.c
+++ Prova-1/Lista-4/q14.c
@@ -9,7 +9,7 @@ int main(int argc, char const *argv[])
 
 
     //Nenhum numero divide por um que é mais da metade dele mesmo
-    for (int i = 1; i <= num / 2; i++)
+    for (i = 1; i <= num / 2; i++)
     {
         if (num % i == 0)
         {
