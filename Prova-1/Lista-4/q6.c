c99a5e53b6f3944e3a1b897425b4b30078320a88
IGOR <igor2311@gmail.com>
2022-09-15 22:26:19 -0300

    Diagonal principal

--- Prova-1/Lista-4/q6.c
+++ Prova-1/Lista-4/q6.c
@@ -1,22 +1,21 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int i,j;
+    int i, j;
     int tam;
     printf("Digite o tamanho da matriz: ");
-    scanf("%d",&tam);
+    scanf("%d", &tam);
 
     printf("Diagonal principal: \n");
-    for ( i = 0; i < tam; i++)
+    for (i = 0; i < tam; i++)
     {
         printf("\n");
-        for ( j = 0; j < tam; j++)
+        for (j = 0; j < tam; j++)
         {
-            if(i == j)
-                printf("%d ",j);
+            if (i == j)
+                printf("%d ", j);
         }
-        
     }
-    
+
     return 0;
 }
